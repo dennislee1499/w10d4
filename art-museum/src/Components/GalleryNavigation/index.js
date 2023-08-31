6c5b9da89bc3cab2cda7c4c6224e33841a7cffe3
ajb-4 <alexbrown4711@gmail.com>
2023-08-31 17:26:41 -0400

    last commit of day

--- art-museum/src/Components/GalleryNavigation/index.js
+++ art-museum/src/Components/GalleryNavigation/index.js
@@ -1,15 +1,27 @@
-// import React from "react";
-// import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
+import React from "react";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 
 
-// function GalleryNavigation({galleries}) {
-//     // console.log(galleries)
-//     <nav>
-//       <h1 className="galleries">Galleries</h1>
-//       <NavLink to="/">Homepage </NavLink>
-//     </nav>
-// }
+function GalleryNavigation({galleries}) {
+    console.log(galleries)
+    // return (<nav>
+    //   galleries.map(gallery =>)
+    //   <NavLink to="/">Homepage </NavLink>
+    // </nav>)
+
+    return (
+        <nav>
+            {galleries.map (gallery => {
+                <NavLink key={gallery.id} to={`/galleries/${gallery.id}`}>
+                    {gallery.name}
+                </NavLink>
+            })}
+        </nav>
+    )
+
+
+}
 
 
 
-// export default GalleryNavigation
+export default GalleryNavigation
